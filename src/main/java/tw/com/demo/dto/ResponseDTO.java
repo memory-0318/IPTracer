e870a2081c2c4bbc1b4ad0a1bd06f2b58a247e2b
Brian Su <brian.su@tpisoftware.com>
2021-07-13 23:29:00 +0800

    feat: 增加ResponseDTO

---
+++ src/main/java/tw/com/demo/dto/ResponseDTO.java
@@ -0,0 +1,49 @@
+package tw.com.demo.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+/**
+ * @author Brian Su <brian.su@tpisoftware.com>
+ * @description:
+ * @date: 2021/7/13
+ */
+@Data
+@Setter(value = AccessLevel.NONE)
+@Builder(setterPrefix = "set")
+public class ResponseDTO <T> {
+    private Boolean success;
+    private OffsetDateTime dateTime;
+    private T data;
+    private String errorMsg;
+
+    public static ResponseDTO<Object> ok() {
+        return ResponseDTO.ok(null);
+    }
+
+    public static ResponseDTO<Object> ok(Object data) {
+        return ResponseDTO.builder()
+            .setSuccess(true)
+            .setDateTime(OffsetDateTime.now())
+            .setData(data)
+            .setErrorMsg(null)
+            .build();
+    }
+
+    public static ResponseDTO<Object> fail() {
+        return ResponseDTO.fail("Unknown error");
+    }
+
+    public static ResponseDTO<Object> fail(String errorMsg) {
+        return ResponseDTO.builder()
+            .setSuccess(false)
+            .setDateTime(OffsetDateTime.now())
+            .setData(null)
+            .setErrorMsg(errorMsg)
+            .build();
+    }
+}
