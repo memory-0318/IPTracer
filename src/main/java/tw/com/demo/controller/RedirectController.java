668439917878b63434e39dc51c30eeb9d4662ead
Brian Su <brian.su@tpisoftware.com>
2021-07-25 12:58:21 +0800

    chore: 調整RedirectController內容，先暫時forward到www.google.com，並移除多餘的依賴

--- src/main/java/tw/com/demo/controller/RedirectController.java
+++ src/main/java/tw/com/demo/controller/RedirectController.java
@@ -1,13 +1,11 @@
 package tw.com.demo.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import tw.com.demo.service.UrlPathDecEncService;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -18,10 +16,7 @@ import java.net.URISyntaxException;
  * @date: 2021/7/7
  */
 @Controller
-@RequiredArgsConstructor
 public class RedirectController {
-    private final UrlPathDecEncService urlPathDecEncService;
-
     @GetMapping(value = { "/{encodedUrlPath}" })
     public String redirectMiddlePage(@PathVariable("encodedUrlPath") String encodedUrlPath) {
         return "index";
@@ -30,7 +25,7 @@ public class RedirectController {
     @GetMapping("/reurl/{encodedUrlPath}/check")
     public ResponseEntity<Void> greeting(@PathVariable("encodedUrlPath") String encodedUrlPath) {
         try {
-            URI realUri = new URI(this.urlPathDecEncService.decodeUrlPath(encodedUrlPath));
+            URI realUri = new URI("https://www.google.com");
 
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.setLocation(realUri);
